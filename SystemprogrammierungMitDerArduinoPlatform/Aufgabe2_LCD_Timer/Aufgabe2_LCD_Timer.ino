a325e69bf91c7db2ffe1fa70b3fc2cf7501aae33
Arne Schilling <arne.schilling@beuth-hochschule.de>
2015-10-13 14:58:52 +0200

    Beuth-Kurs Arduino. Aufgabe 2, Version 1b: Mit Fehlern lauffaehig. Ausgabe noch auf Serial Monitor. Buttons ueber Interrupts.

--- SystemprogrammierungMitDerArduinoPlatform/Aufgabe2_LCD_Timer/Aufgabe2_LCD_Timer.ino
+++ SystemprogrammierungMitDerArduinoPlatform/Aufgabe2_LCD_Timer/Aufgabe2_LCD_Timer.ino
@@ -3,24 +3,27 @@
  * Shows a stop watch with a 1/100th seconds precision on a 4-line lcd.
  * The running timer is displayed in the first line.
  * The second to fourth line displays three split times taken when a (green) button is pressed.
- * By pressing a (red) button the stop watch will be reset. All split times will be cleared an 
+ * By pressing a (red) button the stop watch will be reset. All split times will be cleared and
  * the timer restarts from zero.
  */
 
-// Pin assignment
-int pinResetButton = 2;     // The red button
-int pinSplitTimeButton = 3; // The green button
+ // Bei 0:1:5:53 passiert ein ungeplanter reset.
 
-// Split times in milliseconds
-volatile int splitTime1 = 0;
-volatile int splitTime2 = 0;
-volatile int splitTime3 = 0;
+// Pin assignment
+const int pinResetButton = 2;     // The red button
+const int pinSplitTimeButton = 3; // The green button
+
+// Multiple split times in milliseconds
+const int numSplitTimes = 3;
+volatile unsigned long splitTime[numSplitTimes];
 
 // The instant of time of the last reset (difference bewteen current timer and uptime of the microcontroller)
 // in milliseconds
-volatile int resetTime = 0;
+volatile unsigned long resetTime = 0;
 
 void setup() {
+  // Initialize timer/splitTimes
+  resetSplitTimes();
   // Initialize serial and wait for port to open:
   Serial.begin(9600);
   while (!Serial) {
@@ -35,20 +38,28 @@ void setup() {
 
 void loop() {
   timer();
+  delay(10);
 }
 
 /*
  * Reset the timer and clear all split times.
  */
 void resetTimer() {
-  // delete previous times
-  splitTime1 = 0;
-  splitTime2 = 0;
-  splitTime3 = 0;
-  // clear LCDisplay
-  // TODO
   // reset timer
   resetTime = millis();
+  // delete previous times
+  resetSplitTimes();
+  // clear LCDisplay
+//  lcd.clear();
+}
+
+/*
+ * Delete all previously taken split times.
+ */
+void resetSplitTimes() {
+  for (int i = 0; i < numSplitTimes; i++) {
+    splitTime[i] = 0;
+  }
 }
 
 /*
@@ -56,10 +67,11 @@ void resetTimer() {
  */
 void takeSplitTime() {
   // Shift taken split times
-  splitTime3 = splitTime2;
-  splitTime2 = splitTime1;
+  for (int i = numSplitTimes; i > 0; i--) {
+    splitTime[i] = splitTime[i-1];
+  }
   // Take new split time
-  splitTime1 = currentTime();
+  splitTime[0] = currentTime();
   // Display split times
   printSplitTimes();
 }
@@ -67,7 +79,7 @@ void takeSplitTime() {
 /*
  * Return time in milliseconds since last timer reset
  */
- int currentTime() {
+ unsigned long currentTime() {
   return millis() - resetTime;
  }
 
@@ -76,35 +88,35 @@ void takeSplitTime() {
  * The time format is hh:mm:ss.hun
  */
 void timer() {
-  int hundredth;
-  int seconds;
-  int minutes;
-  int hours;
-  getCurrentTime(int &hundredth, int &seconds, int &minutes, int &hours);
-  printTime(int hundredth, int seconds, int minutes, int hours);
+  unsigned long hundredth;
+  unsigned long seconds;
+  unsigned long minutes;
+  unsigned long hours;
+  getCurrentTime(&hundredth, &seconds, &minutes, &hours);
+  printTime(hundredth, seconds, minutes, hours);
 }
 
 /*
  * Get the elapsed time in hours, minutes, seconds and hundredth since the last timer reset.
  */
-void getCurrentTime(int *hundredth, int *seconds, int *minutes, int *hours) {
-  int currentTime = currentTime();
-  int totalSeconds = currentTime / 1000;
-  *hundredth = (currentTime / 10) % 100;
+void getCurrentTime(unsigned long *hundredth, unsigned long *seconds, unsigned long *minutes, unsigned long *hours) {
+  unsigned long cTime = currentTime();
+  unsigned long totalSeconds = cTime / 1000;
+  *hundredth = (cTime / 10) % 100;
   *seconds = totalSeconds % 60;
   *minutes = (totalSeconds / 60) % 60;
   *hours = (totalSeconds / 3600);
 }
 
-void printTime(int hundredth, int seconds, int minutes, int hours) {
-  Serial.println("");
-  Serial.print(hours);
+void printTime(unsigned long hundredth, unsigned long seconds, unsigned long minutes, unsigned long hours) {
+  //Serial.println("");
+  Serial.print(hours, DEC);
   Serial.print(":");
-  Serial.print(minutes);
+  Serial.print(minutes, DEC);
   Serial.print(":");
-  Serial.print(seconds);
+  Serial.print(seconds, DEC);
   Serial.print(".");
-  Serial.println(hundredth);
+  Serial.println(hundredth, DEC);
 }
 
 void printSplitTimes() {
